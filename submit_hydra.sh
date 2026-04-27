c089c4e9f8b5585c9fc3b2f0269c6470f435f268
Emrik Erikson <emrike@alvis2.int.private>
2026-04-27 15:59:59 +0200

    Old run check

--- submit_hydra.sh
+++ submit_hydra.sh
@@ -1,8 +1,29 @@
 #!/bin/bash
+
 CONFIG_NAME=$1
+
 mkdir -p outputs/slurm_logs
 
+sbatch <<EOF
+#!/bin/bash
+#SBATCH -J ${CONFIG_NAME}
+#SBATCH --gpus-per-node=T4:1 # -C 2xV100 # For 2x RAM?
+#SBATCH --time=02:00:00
+#SBATCH -o outputs/slurm_logs/${CONFIG_NAME}_%j.log
+#SBATCH --account=naiss2025-22-762
+
 export OMP_NUM_THREADS=1
+
+# Activate environment
+source .venv/bin/activate
+
+# Install the necessary packages
+python3 -m pip install -e .
+
 export PYTHONUNBUFFERED=1
 
+# Run the Python script with the config file
 time torchrun --standalone --nproc_per_node=1 run_hydra.py -cn $@
+#### srun -u python3 -u run_hydra.py -cn $@
+EOF
+
