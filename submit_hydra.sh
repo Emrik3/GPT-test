46c28754a9a7f77d4ec8a003e9d4c68d983a6b9c
Emrik Erikson <emrike@alvis2.int.private>
2026-04-29 11:30:22 +0200

    fix sweep args

--- submit_hydra.sh
+++ submit_hydra.sh
@@ -7,16 +7,10 @@ mkdir -p outputs/slurm_logs
 sbatch <<EOF
 #!/bin/bash
 #SBATCH -J ${CONFIG_NAME}
-#SBATCH --ntasks-per-node=4
-#SBATCH --gpus-per-node=4
-#SBATCH --cpus-per-gpu=8
-#SBATCH --time=23:59:00
-#SBATCH --constraint=h100
-#SBATCH --mem=80G
-#SBATCH --nodes=1 
-#SBATCH --partition=gpu
-#SBATCH --exclude=workergpu027
+#SBATCH --gpus-per-node=T4:1
+#SBATCH --time=01:00:00
 #SBATCH -o outputs/slurm_logs/${CONFIG_NAME}_%j.log
+#SBATCH --account=naiss2025-22-762
 
 module load python
 export OMP_NUM_THREADS=1
@@ -30,6 +24,6 @@ python3 -m pip install -e .
 export PYTHONUNBUFFERED=1
 
 # Run the Python script with the config file
-time torchrun --standalone --nproc_per_node=4 run_hydra.py -cn $@
+time torchrun --standalone --nproc_per_node=1 run_hydra.py -cn $@
 #### srun -u python3 -u run_hydra.py -cn $@
 EOF
