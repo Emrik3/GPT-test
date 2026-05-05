7dd5d43d626d06969dbb52f5966fe92b92c9bbe9
Emrik Erikson <emrike@alvis2.int.private>
2026-05-05 10:18:53 +0200

    correct main run small 1B

--- sweeps/sweep1.sh
+++ sweeps/sweep1.sh
@@ -5,7 +5,7 @@ MUON="optimizer_params.name=muon +optimizer_params.args.ns_steps=5"
 for wd in 0; do
 
 for lr in 0.001 0.002 0.005 0.01 0.02; do
-for polar_method in Keller polarexpress machpolar5 machpolar9 machpolar17; do
+for polar_method in Keller polarexpress machpolar5 machpolar9 machpolar17 machpolar172; do
 ./submit_hydra.sh test_hydra $COMMON $MUON +optimizer_params.args.polar_method=$polar_method optimizer_params.args.lr=$lr optimizer_params.args.weight_decay=$wd $WANDB
 done
 done
