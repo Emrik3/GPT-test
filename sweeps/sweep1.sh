c36440c9341d713d0beff39f9e8a2e7f61a9f590
Emrik Erikson <emrike@alvis2.int.private>
2026-05-14 12:15:15 +0200

    Adding adamw runs and keller for the existing plots

--- sweeps/sweep1.sh
+++ sweeps/sweep1.sh
@@ -1,15 +1,15 @@
 WANDB="+logging_params.wandb.project=polar-express"
-COMMON="+training_data=fineweb gpt_model=gpt-small hydra.job.name=\"main_run\""
+COMMON="+training_data=fineweb gpt_model=gpt-small hydra.job.name=\"main_run_wdon\""
 MUON="optimizer_params.name=muon +optimizer_params.args.ns_steps=5"
 
 #Do now run with weight decay on
-for wd in 0.0; do
+for wd in 0.1; do
 
-#for lr in 0.001 0.005 0.01; do
-#for polar_method in Keller; do
-#./submit_hydra.sh test_hydra $COMMON $MUON +optimizer_params.args.polar_method=$polar_method optimizer_params.args.lr=$lr optimizer_params.args.weight_decay=$wd $WANDB
-#done
-#done
+for lr in 0.001 0.005 0.01; do
+for polar_method in Keller; do
+./submit_hydra.sh test_hydra $COMMON $MUON +optimizer_params.args.polar_method=$polar_method optimizer_params.args.lr=$lr optimizer_params.args.weight_decay=$wd $WANDB
+done
+done
 
 for lr in 0.0001 0.0002 0.0005 0.001 0.002 0.005; do
 ./submit_hydra.sh test_hydra $COMMON optimizer_params.name=adamw optimizer_params.args.lr=$lr optimizer_params.args.weight_decay=$wd $WANDB
